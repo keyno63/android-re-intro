7c076a23fbd53e1a19956377c260c046c08d1cf7
keyno63 <kofujiw@gmail.com>
2024-08-09 01:25:08 +0900

    update main activity

--- app/src/main/java/tokyo/maigo_name/introduction/MainActivity.kt
+++ app/src/main/java/tokyo/maigo_name/introduction/MainActivity.kt
@@ -1,13 +1,36 @@
 package tokyo.maigo_name.introduction
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.enableEdgeToEdge
+import android.text.TextUtils
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import tokyo.maigo_name.introduction.domain.Task
+import tokyo.maigo_name.introduction.service.TaskAdapter
+import tokyo.maigo_name.introduction.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var taskAdapter: TaskAdapter
+    private val taskList = mutableListOf<Task>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        taskAdapter = TaskAdapter(taskList)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = taskAdapter
+
+        binding.buttonAdd.setOnClickListener {
+            val taskName = binding.editTextTask.text.toString()
+            if (!TextUtils.isEmpty(taskName)) {
+                val task = Task(taskName)
+                taskAdapter.addTask(task)
+                binding.editTextTask.text.clear()
+            }
+        }
     }
 }
-
