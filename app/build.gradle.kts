b98a34898b0f28d5be3cf7a32626d6d0da5ea6e0
keyno63 <kofujiw@gmail.com>
2024-08-16 00:24:14 +0900

    update compile sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "tokyo.maigo_name.introduction"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "tokyo.maigo_name.android.re.introduction"
