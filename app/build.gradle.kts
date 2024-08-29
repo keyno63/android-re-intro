a36383a8a31250d519746fe3b5e64dadb1d7d53e
keyno63 <kofujiw@gmail.com>
2024-08-30 02:42:24 +0900

    version up. add app icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "tokyo.maigo_name.android.re.introduction"
         minSdk = 30
         targetSdk = 34
-        versionName = "0.0.9"
+        versionName = "0.1.0"
         versionCode = fun(v: String): Int {
             val parts = v.split(".")
             return parts[0].toInt() * 10000 + parts[1].toInt() * 100 + parts[2].toInt()
